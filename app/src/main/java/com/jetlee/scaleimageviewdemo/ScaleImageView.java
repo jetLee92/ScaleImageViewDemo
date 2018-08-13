8678e7a8907ff416c6af00223b4e656cc9d88977
jetLee92 <940077208@qq.com>
2018-08-13 16:48:16 +0800

    Initial commit

---
+++ app/src/main/java/com/jetlee/scaleimageviewdemo/ScaleImageView.java
@@ -0,0 +1,25 @@
+package com.jetlee.scaleimageviewdemo;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * @author：Jet啟思
+ * @date:2018/8/13 16:46
+ */
+public class ScaleImageView extends View {
+    public ScaleImageView(Context context) {
+        super(context);
+    }
+
+    public ScaleImageView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public ScaleImageView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+}
